b3cf9075a8d2be27498be938ebad7dd16cf6c718
minhle106 <minhletravel106@gmail.com>
2023-04-22 17:34:29 +0700

    UpdateUI

---
+++ view/html/UI_manager/manage_employee.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Management</title>
+    <!-- ======= Styles ====== -->
+      <!--  icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
+      <!--  style -->
+    <link rel="stylesheet" type="text/css" href="view/bootstrap/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="view/css/UI_manager/style_navbar_manager.css"/>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="view/css/UI_employee/style_table_employee.css">
+    <!-- ======= Scripts ====== -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+
+    <script>
+      function showPassword(id) {
+        var x = document.getElementById(id);
+        if (x.type === "password") {
+          x.type = "text";
+        } else {
+          x.type = "password";
+        }
+      }
+      //get content using ajax
+      /* function getMainContent(link, elementID) {
+          $.ajax({
+              // The link we are accessing.
+              url: link,
+                  
+              // The type of request.
+              type: "get",
+                  
+              // The type of data that is getting returned.
+              dataType: "html",
+              success: function( strData ){
+                  document.getElementById(elementID).innerHTML = strData;
+                  carousel()
+              }
+          });
+      } */
+    </script>
+
+</head>
+
+<body>
+    <?php
+      if (isset($errResultAdd)) {
+        $errResultAdd = json_decode($errResultAdd);
+      }
+      if (isset($errResultUpd)) {
+        $errResultUpd = json_decode($errResultUpd);
+      }
+    ?>
+
+    <div class="container-fluid">
+      <!-- Topbar Start -->
+      <?php include 'component/topbar.php'; ?>
+      <!-- Topbar End -->
+
+      <div class="row">
+          <!-- menu bên trái -->
+          <div class="col-md-3 col-lg-2 sidebar" style="margin-top: 60px;">
+            <div class="sidebar-sticky">
+              <ul class="nav flex-column">
+                <li class="nav-item">
+                  <a class="nav-link" href="index.php?controller=manager&action=home_page_manager">Thông tin chung</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" href="index.php?controller=manager&action=manage_employee">Nhân viên</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Sản phẩm</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Đơn hàng</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Feedback</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Doanh thu</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-md-9 col-lg-10 mt-3">
+            <div class="container-fluid mt-5 pt-3">
+              <div class="row">
+                <div class="col-md-12 mb-3">
+                  <div class="card">
+                    <div class="card-header">
+                      <span><i class="bi bi-table me-2"></i></span> Data Table
+                    </div>
+                    <div class="card-body">
+                      <!-- Employee Table Start -->
+                      <?php include 'component/employee_table.php'; ?>
+                      <!-- Employee Table End -->
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
+
+    <!-- ======= Scripts ====== -->
+    <script src="view/script/employee_table.js"></script>
+
+    <?php
+      /*  if (isset($errResultAdd)) echo '
+      <script>
+        var myModal = new bootstrap.Modal(document.getElementById("confirm-add-modal"), {
+          keyboard: false
+        })
+        myModal.show();
+      </script>
+      ';
+
+      if (isset($errResultUpd)) echo '
+      <script>
+        var myModal = new bootstrap.Modal(document.getElementById("confirm-edit-modal-'.$curID.'"), {
+          keyboard: false
+        })
+        myModal.show();
+      </script>
+      ';  */
+    ?>
+    
+</body>
+
+</html>
---
+++ view/html/UI_user/component/topbar.php
@@ -0,0 +1,143 @@
+<!-- Style Topbar -->
+<style>
+    @media (min-width: 320px) {
+        .row .col-mobile-1{
+            flex: 0 0 auto;
+            width: 10%;
+        }
+        .row .col-mobile-2{
+            flex: 0 0 auto;
+            width: 20%;
+        }
+        .row .col-mobile-3{
+            flex: 0 0 auto;
+            width: 30%;
+        }
+        .row .col-mobile-4{
+            flex: 0 0 auto;
+            width: 40%;
+        }
+        .row .col-mobile-5{
+            flex: 0 0 auto;
+            width: 50%;
+        }
+        .row .col-mobile-6{
+            flex: 0 0 auto;
+            width: 60%;
+        }
+        .row .col-mobile-7{
+            flex: 0 0 auto;
+            width: 70%;
+        }
+        .row .col-mobile-8{
+            flex: 0 0 auto;
+            width: 80%;
+        }
+        .row .col-mobile-9{
+            flex: 0 0 auto;
+            width: 90%;
+        }
+        .row .col-mobile-10{
+            flex: 0 0 auto;
+            width: 100%;
+        }
+    }
+    @media (min-width: 768px) {
+        .row .col-tablet-1{
+            flex: 0 0 auto;
+            width: 10%;
+        }
+        .row .col-tablet-2{
+            flex: 0 0 auto;
+            width: 20%;
+        }
+        .row .col-tablet-3{
+            flex: 0 0 auto;
+            width: 30%;
+        }
+        .row .col-tablet-4{
+            flex: 0 0 auto;
+            width: 40%;
+        }
+        .row .col-tablet-5{
+            flex: 0 0 auto;
+            width: 50%;
+        }
+        .row .col-tablet-6{
+            flex: 0 0 auto;
+            width: 60%;
+        }
+        .row .col-tablet-7{
+            flex: 0 0 auto;
+            width: 70%;
+        }
+        .row .col-tablet-8{
+            flex: 0 0 auto;
+            width: 80%;
+        }
+        .row .col-tablet-9{
+            flex: 0 0 auto;
+            width: 90%;
+        }
+        .row .col-tablet-10{
+            flex: 0 0 auto;
+            width: 100%;
+        }
+    }
+
+    .btn .badge{
+        color: black;
+    }
+    .form-control {
+        border-radius: 0;
+    }
+    .input-group-append .btn {
+        border-radius: 0;   
+        color: rgb(179, 93, 107);
+    }
+    .navbar_ic .btn{
+        color: rgb(179, 93, 107);
+    }
+</style>
+<div class="container-fluid">
+    <div class="row align-items-center" style="background-color: #f2f2f2;  height:80px;">
+        <div class="col-lg-3 d-none d-lg-block px-5">
+            <a href="index.php?controller=user&action=home_page_user">
+                <img src="view/images/logo.jpg" style="width: 150px;" alt="logo">
+            </a>
+        </div>
+        <div class="col-lg-9">
+            <div class="row">
+                <div class="col-tablet-7 col-mobile-5">
+                    <form class="form-inline" style="margin-right: 1%;">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for products" style="border-radius: 0;">
+                        <div class="input-group-append">
+                        <button class="btn border btn-outline-secondary" type="button">
+                            <i class="fa fa-search"></i>
+                        </button>
+                        </div>
+                    </div>
+                    </form>
+                </div>
+                <div class="col-tablet-3 col-mobile-5 navbar_ic d-flex justify-content-end">
+                    <button type="button" class="btn border btn-outline-secondary"
+                        data-toggle="modal" data-target="#exampleModal"
+                        style="margin-right: 1%; border-radius: 0;">
+                        <div class="d-flex justify-content-end">
+                            <i class="bi bi-calendar-plus me-2"></i>
+                            <span style="font-weight: 500; color: black">Đặt Bàn</span>
+                        </div>
+                    </button>
+                    <!-- Modal -->
+                    <?php include 'modal_booking.php'; ?>
+                    <!-- Modal -->
+
+                    <!-- Cart Popup Start -->
+                    <?php include 'cart_dropdown.php'; ?>
+                    <!-- Cart Popup End -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
